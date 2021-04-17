81940c8ca7c4fab80673609594315b726f08e44c
Axel Sverker Ahdritz <ahdritzaxel@gmail.com>
2021-04-17 14:07:00 -0700

    updating avltree branch to merge

--- containers/BST.py
+++ containers/BST.py
@@ -8,6 +8,7 @@ class BST(BinaryTree):
         if xs is not None:
             for x in xs:
                 self.insert(x)
+        self.root = None
 
     def __repr__(self):
         return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'

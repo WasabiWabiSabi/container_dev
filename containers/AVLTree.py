ebe2797daf1cf0695d6b61848f203fa105d94c59
Axel Sverker Ahdritz <ahdritzaxel@gmail.com>
2021-04-18 19:02:13 -0700

    finish homework attempt 1

--- containers/AVLTree.py
+++ containers/AVLTree.py
@@ -1,100 +1,95 @@
-'''
-This file implements the AVL Tree data structure.
-The functions in this file are considerably harder than the functions in the BinaryTree and BST files,
-but there are fewer of them.
-'''
-
 from containers.BinaryTree import BinaryTree, Node
 from containers.BST import BST
 
 
-class AVLTree():
-    '''
-    FIXME:
-    AVLTree is currently not a subclass of BST.
-    You should make the necessary changes in the class declaration line above
-    and in the constructor below.
-    '''
+class AVLTree(BST):
 
     def __init__(self, xs=None):
-        '''
-        FIXME:
-        Implement this function.
-        '''
+        super().__init__()
+        if xs is not None:
+            for x in xs:
+                self.insert(x)
+        self.root = None
 
     def balance_factor(self):
-        '''
-        Returns the balance factor of a tree.
-        '''
         return AVLTree._balance_factor(self.root)
 
     @staticmethod
     def _balance_factor(node):
-        '''
-        Returns the balance factor of a node.
-        '''
         if node is None:
             return 0
         return BinaryTree._height(node.left) - BinaryTree._height(node.right)
 
     def is_avl_satisfied(self):
-        '''
-        Returns True if the avl tree satisfies that all nodes have a balance factor in [-1,0,1].
-        '''
         return AVLTree._is_avl_satisfied(self.root)
 
     @staticmethod
     def _is_avl_satisfied(node):
-        '''
-        FIXME:
-        Implement this function.
-        '''
+        if node is None:
+            return True
+        if AVLTree._balance_factor(node) in range(-1, 1):
+            AVLTree._is_avl_satisfied(node.left)
+            AVLTree._is_avl_satisfied(node.right)
+        else:
+            return False
 
     @staticmethod
     def _left_rotate(node):
-        '''
-        FIXME:
-        Implement this function.
-
-        The lecture videos provide a high-level overview of tree rotations,
-        and the textbook provides full python code.
-        The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
-        however, so you will have to adapt their code.
-        '''
+        if node is None or node.right is None:
+            return node
+        new_root = Node(node.right.value)
+        new_root.right = node.right.right
+        new_root.left = Node(node.value)
+        new_root.left.left = node.left
+        if node.right.left:
+            new_root.left.right = node.right.left
+        return new_root
 
     @staticmethod
     def _right_rotate(node):
-        '''
-        FIXME:
-        Implement this function.
-
-        The lecture videos provide a high-level overview of tree rotations,
-        and the textbook provides full python code.
-        The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
-        however, so you will have to adapt their code.
-        '''
+        if node is None or node.left is None:
+            return node
+        new_root = Node(node.left.value)
+        new_root.left = node.left.left
+        new_root.right = Node(node.value)
+        new_root.right.right = node.right
+        if node.left.right:
+            new_root.right.left = node.left.right
+        return new_root
 
     def insert(self, value):
-        '''
-        FIXME:
-        Implement this function.
+        self._insert(self.root, value)
+        if not self.is_avl_satisfied():
+            self._rebalance(self.root)
+        return
 
-        The lecture videos provide a high-level overview of how to insert into an AVL tree,
-        and the textbook provides full python code.
-        The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
-        however, so you will have to adapt their code.
-
-        HINT:
-        It is okay to add @staticmethod helper functions for this code.
-        The code should look very similar to the code for your insert function for the BST,
-        but it will also call the left and right rebalancing functions.
-        '''
+    @staticmethod
+    def _insert(node, value):
+        if node.value == value:
+            return
+        if value > node.value:
+            if node.right:
+                AVLTree._insert(node.right, value)
+            else:
+                node.right = Node(value)
+        if value < node.value:
+            if node.left:
+                AVLTree._insert(node.left, value)
+            else:
+                node.left = Node(value)
 
     @staticmethod
     def _rebalance(node):
-        '''
-        There are no test cases for the rebalance function,
-        so you do not technically have to implement it.
-        But both the insert function needs the rebalancing code,
-        so I recommend including that code here.
-        '''
+        bf = AVLTree._balance_factor(node)
+        if bf < 0:
+            if AVLTree._balance_factor(node.right) > 0:
+                AVLTree._right_rotate(node.right)
+                AVLTree._left_rotate(node)
+            else:
+                AVLTree._left_rotate(node)
+        elif bf > 0:
+            if AVLTree._balance_factor(node.left) < 0:
+                AVLTree._left_rotate(node.left)
+                AVLTree._right_rotate(node)
+            else:
+                AVLTree._right_rotate(node)
--- containers/BST.py
+++ containers/BST.py
@@ -14,20 +14,9 @@ class BST(BinaryTree):
         return type(self).__name__ + '(' + str(self.to_list('inorder')) + ')'
 
     def __eq__(self, t2):
-        '''
-        This method checks to see if the contents of self and t2 are equal.
-        The expression `a == b` desugars to `a.__eq__(b)`.
-
-        NOTE:
-        We only care about "semantic" equality,
-        and not "syntactic" equality.
-        That is, we do not care about the tree structure itself,
-        and only care about the contents of what the tree contains.
-
-        HINT:
-        Convert the contents of both trees into a sorted list,
-        then compare those sorted lists for equality.
-        '''
+        in_order1 = BinaryTree.to_list(self, 'inorder')
+        in_order2 = BinaryTree.to_list(t2, 'inorder')
+        return in_order1 == in_order2
 
     def is_bst_satisfied(self):
         if self.root:

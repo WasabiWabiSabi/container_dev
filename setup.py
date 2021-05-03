36fe44a592b1e9ed5b06cdfea290889692b60808
Axel Sverker Ahdritz <ahdritzaxel@gmail.com>
2021-05-03 00:34:01 -0700

    "merge stuff!"

--- setup.py
+++ setup.py
@@ -4,19 +4,14 @@ import sys
 from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
-# The directory containing this file
-
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
     with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
         README = f.read()
 except FileNotFoundError:
     README = "Containers from my Data Structures class"
 
-# This call to setup() does all the work
 setup(
     name="cmc_csci046_Wasabi_containers",
     version="1.0.0",
@@ -25,9 +20,9 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/WasabiWabiSabi/container_dev",
     author="WasabiWabiSabi",
-    license="GPL",
+    license="MIT",
     classifiers=[
-        "LLicense :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],

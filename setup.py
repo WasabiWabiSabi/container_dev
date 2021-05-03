0346473f85e31f57cd7730b5673c41400ed6c2e4
Axel Sverker Ahdritz <ahdritzaxel@gmail.com>
2021-05-02 23:59:02 -0700

    new setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-import pathlib
-from setuptools import find_packages, setup
+import io
+import os
+import sys
+from shutil import rmtree
+from setuptools import find_packages, setup, Command
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
-README = (HERE / "README.md").read_text()
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        README = f.read()
+except FileNotFoundError:
+    README = "Containers from my Data Structures class"
 
 # This call to setup() does all the work
 setup(
@@ -13,7 +22,7 @@ setup(
     version="1.0.0",
     description="Containers from my Data Structures class, including Binary Tree, Binary Search Tree, the AVL Tree, and Heap!",
     long_description=README,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
     url="https://github.com/WasabiWabiSabi/container_dev",
     author="WasabiWabiSabi",
     license="GPL",
@@ -24,6 +33,4 @@ setup(
     ],
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
-        ]
-    },
 )
